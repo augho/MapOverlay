ed2210809e4b6eb83558e44c475082289671c6b4
augho <aughouba@gmail.com>
2024-02-07 19:46:44 +0100

    basic styling and menu swap

--- src/main/java/com/sdd/mapoverlay/HelloApplication.java
+++ src/main/java/com/sdd/mapoverlay/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+// https://coolors.co/palette/03045e-0077b6-00b4d8-90e0ef-caf0f8
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
--- src/main/java/com/sdd/mapoverlay/SidePanelController.java
+++ src/main/java/com/sdd/mapoverlay/SidePanelController.java
@@ -1,4 +1,44 @@
 package com.sdd.mapoverlay;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 public class SidePanelController {
+    public VBox swapContainer;
+    public Button playButton;
+    public Button editButton;
+
+    @FXML
+    protected void onPlayButtonCLick() {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                SidePanelController.class.getResource("play-panel.fxml")
+        );
+        playButton.setDisable(true);
+        editButton.setDisable(false);
+        try {
+            swapContainer.getChildren().set(0, fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("[ERR]Couldn't load edit-panel");
+        }
+
+    }
+    @FXML
+    protected void onEditButtonCLick() {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                SidePanelController.class.getResource("edit-panel.fxml")
+        );
+        playButton.setDisable(false);
+        editButton.setDisable(true);
+        try {
+            swapContainer.getChildren().set(0, fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("[ERR]Couldn't load edit-panel");
+        }
+    }
 }
