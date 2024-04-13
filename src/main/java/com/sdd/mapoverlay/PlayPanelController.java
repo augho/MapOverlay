0e6e63a1d1c8caca863f800091eba90d7a6210d0
BolBoBul <211917@umons.ac.be>
2024-04-13 18:26:21 +0200

    added a backward/forward motion to avoid clicking

--- src/main/java/com/sdd/mapoverlay/PlayPanelController.java
+++ src/main/java/com/sdd/mapoverlay/PlayPanelController.java
@@ -1,10 +1,14 @@
 package com.sdd.mapoverlay;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +22,10 @@ import com.sdd.mapoverlay.utils.Store;
 
 public class PlayPanelController {
 
-    public int currentStep = 0;
-    public ArrayList<Intersection> pointsToPlace = new ArrayList<>();
-    @FXML
-    public Button playButton;
+    public static int currentStep;
+    public static ArrayList<Intersection> pointsToPlace = new ArrayList<>();
+    private Timeline timelineFwd, timelineBwd;
+
     @FXML
     public CheckBox displayOverlayCheckBox;
     @FXML
@@ -29,17 +33,24 @@ public class PlayPanelController {
     @FXML
     public Button nextStepButton;
     @FXML
-    public Label labelIntersection = new Label();
+    public Label labelIntersection = new Label("No intersection found");
 
 
     @FXML
     protected void onPlayButtonClick() throws FileNotFoundException{
+        
+        /* System.out.println(pointsToPlace.size());
+        System.out.println(currentStep); */
+
         // Reset the current step and disable the prevStepButton since you can't go before step 0
         currentStep = 0;
-        prevStepButton.setDisable(true);
 
         pointsToPlace = computeOverlay();
         
+        // We reset the chart to remove the previous intersection points
+        System.out.println(Store.getRootController().lineChart.getData().size());
+        Store.getRootController().lineChart.getData().remove(Store.getRootController().getSerieByName("Intersection_Points"));
+        System.out.println("Intersection points series removed");
         Store.getRootController().addIntersectionPoints(pointsToPlace);
 
         Store.getRootController().addSweepLine();
@@ -59,8 +70,11 @@ public class PlayPanelController {
     protected void onPrevStepButtonClick() {
         currentStep--;
         Store.getRootController().highlightIntersectionPoint(pointsToPlace.get(currentStep).p());
-        if (currentStep == 0) {
+        if (currentStep <= 0) {
             prevStepButton.setDisable(true);
+            currentStep = 0;
+        } else {
+            prevStepButton.setDisable(false);
         }
         if (currentStep == pointsToPlace.size() - 2){
             nextStepButton.setDisable(false);
@@ -71,10 +85,12 @@ public class PlayPanelController {
     @FXML
     protected void onNextStepButtonClick() {
         currentStep++;
+        if (currentStep == pointsToPlace.size())
+            currentStep--;
         Store.getRootController().highlightIntersectionPoint(pointsToPlace.get(currentStep).p());
-        if (currentStep == 1) {
+        if (currentStep != 0)
             prevStepButton.setDisable(false);
-        }
+
         if (currentStep == pointsToPlace.size() - 1){
             nextStepButton.setDisable(true);
         }
@@ -100,6 +116,12 @@ public class PlayPanelController {
             XYChart.Series<Number, Number> serie = Store.getRootController().getSerieByName("Intersection_Points");
             if (serie != null) {
                 serie.getNode().setVisible(visibility);
+                for (XYChart.Data<Number, Number> data : serie.getData()) {
+                    if (data.getNode() != null) {
+                        data.getNode().toFront();
+                        data.getNode().setVisible(visibility);
+                    }
+                }
             }
             // Hide or show all the data points
             for (XYChart.Data<Number, Number> data : serie.getData()) {
@@ -116,4 +138,39 @@ public class PlayPanelController {
     protected void onDisplayOverlayClick() {
         setSeriesVisibility(displayOverlayCheckBox.isSelected());
     }
+
+    @FXML
+    protected void onNextStepButtonPressed() {
+        onNextStepButtonClick();
+    }
+
+    @FXML
+    protected void onPrevStepButtonPressed() {
+        onPrevStepButtonClick();
+    }
+
+    @FXML
+    public void initialize() {
+        timelineFwd = new Timeline(new KeyFrame(Duration.millis(80), event -> onNextStepButtonPressed()));
+        timelineBwd = new Timeline(new KeyFrame(Duration.millis(80), event -> onPrevStepButtonPressed()));
+        timelineFwd.setCycleCount(Animation.INDEFINITE);
+        timelineBwd.setCycleCount(Animation.INDEFINITE);
+
+        nextStepButton.setOnMousePressed(event -> {
+            timelineFwd.play();
+        });
+
+        nextStepButton.setOnMouseReleased(event -> {
+            timelineFwd.stop();
+        });
+
+        prevStepButton.setOnMousePressed(event -> {
+            timelineBwd.play();
+        });
+
+        prevStepButton.setOnMouseReleased(event -> {
+            timelineBwd.stop();
+        });
+    }
+    
 }
